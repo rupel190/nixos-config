810cc4f9947836b366f4fece68fb4c33149777aa
rupel <23055682+rupel190@users.noreply.github.com>
2026-03-03 17:22:55 +0100

    Set config to backup duplicate files as .bak

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -8,6 +8,7 @@
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
   home-manager = {
+    backupFileExtension = "bak";
     useUserPackages = true;
     useGlobalPkgs = true;
     extraSpecialArgs = { inherit inputs username host; };
